bf5386f59c8887b1f87bf0656bfa80a90a89db0a
Andrey <starkovvandrey@gmail.ru>
2021-11-17 09:31:51 +0700

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Collections;
+using System.Security.Policy;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public string Allname="";
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            using (StreamWriter sw = new StreamWriter("Students.txt", true))
+            {
+                sw.WriteLine(Student.Text+ ';' +Itemname.Text + ';' +Semester.Text + ';' + result.Text + ';' + Itemtype.Text + ';' + Fullname.Text +';');
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (Student1.Text == "")
+            {
+                MessageBox.Show("Нет введённых данных");
+                return;
+            }
+            Allname = "";
+            string str;
+            bool temp = false;
+            using (StreamReader sw = new StreamReader("Students.txt"))
+            {
+                string FullName = "";
+                char c;
+                StudentBook studentBook = new StudentBook();
+                while (!sw.EndOfStream)
+                {
+
+                    str = "";
+                    while ((c = Convert.ToChar(sw.Read())) != ';')
+                    {
+                        str += c;
+                    }
+
+
+                    if (str == Student1.Text)
+                    {
+                        temp = true;
+                        string type="";
+                        string result="";
+                        string NumberS="";
+                        string Name="";
+                        FullName = "";
+
+                        while ((c = Convert.ToChar(sw.Read())) != ';')
+                        {
+                            Name += c;
+                        }
+
+                        while ((c = Convert.ToChar(sw.Read())) != ';')
+                        {
+                            NumberS += c;
+                        }
+
+                        while ((c = Convert.ToChar(sw.Read())) != ';')
+                        {
+                            result += c;
+                        }
+
+                        while ((c = Convert.ToChar(sw.Read())) != ';')
+                        {
+                            type += c;
+                        }
+
+                        studentBook.Add(type,Convert.ToInt32(result),Convert.ToInt32(NumberS),Name);
+                        Allname += "Тип: " + type +" " + "Оценка: " + result + " " + "Номер семестра: " + NumberS + " " + "Название предмета: " + Name + " " + "\n";
+
+
+                        while ((c = Convert.ToChar(sw.Read())) != ';')
+                        {
+                            FullName += c;
+                        }
+                        sw.ReadLine();
+                        
+                    }
+                    else
+                    {
+
+                        sw.ReadLine();
+                    }
+                }
+
+                if (!temp)
+                {
+                    MessageBox.Show("Данный студент не был найден");
+                    return;
+                }
+                Dyplom.Content = studentBook.Diplom()  ;
+                Stipendia.Content = studentBook.Stipendia();
+                Average.Content = "Средний бал: "+studentBook.average();
+                LbFullname.Content = "Полное Имя: " + FullName;
+
+            }
+
+            Info.IsEnabled = true;
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            string name = Allname;
+            new Window1(name).ShowDialog();
+        }
+
+        private void Allid_Click(object sender, RoutedEventArgs e)
+        {
+            List<string> ForId = new List<string>();
+            List<string> ForName = new List<string>();
+            string name = "";
+            using (StreamReader sw = new StreamReader("Students.txt"))
+            {
+                char c;
+                name = "";
+                while (!sw.EndOfStream)
+                { 
+                    while ((c = Convert.ToChar(sw.Read())) != ';')
+                    {
+                        name += c;
+                    }
+
+                    ForId.Add(name);
+                    name = "";
+
+                    while ((c = Convert.ToChar(sw.Read())) != ';')
+                    {
+                    }
+
+                    while ((c = Convert.ToChar(sw.Read())) != ';')
+                    {
+                    }
+
+                    while ((c = Convert.ToChar(sw.Read())) != ';')
+                    {
+                    }
+
+                    while ((c = Convert.ToChar(sw.Read())) != ';')
+                    {
+                    }
+
+
+                    while ((c = Convert.ToChar(sw.Read())) != ';')
+                    {
+                        name += c;
+                    }
+
+                    ForName.Add(name);
+                    name = "";
+                    sw.ReadLine();
+                }
+            }
+
+            ForName = ForName.Distinct().ToList();
+            ForId= ForId.Distinct().ToList();
+            name = "";
+            for (int i = 0; i < ForId.Count; i++)
+            {
+                name += ForId[i] + "\t" + ForName[i] + "\n";
+            }
+            new Window1(name).ShowDialog();
+        }
+
+        private void currentid_Click(object sender, RoutedEventArgs e)
+        {
+
+            using (StreamReader sw = new StreamReader("Students.txt"))
+            {
+                string FullName = "";
+                char c;
+                StudentBook Clown = new StudentBook();
+                string str = "";
+                bool temp=false;
+                while (!sw.EndOfStream)
+                {
+
+                    str = "";
+                    while ((c = Convert.ToChar(sw.Read())) != ';')
+                    {
+                        str += c;
+                    }
+
+
+                    if (str == inputid.Text)
+                    {
+                        temp = true;
+                        FullName = "";
+
+                        while ((c = Convert.ToChar(sw.Read())) != ';')
+                        {
+                        }
+
+                        while ((c = Convert.ToChar(sw.Read())) != ';')
+                        {
+                        }
+
+                        while ((c = Convert.ToChar(sw.Read())) != ';')
+                        {
+                        }
+
+                        while ((c = Convert.ToChar(sw.Read())) != ';')
+                        {
+                        }
+
+
+                        while ((c = Convert.ToChar(sw.Read())) != ';')
+                        {
+                            FullName += c;
+                        }
+                        sw.ReadLine();
+
+                        Student.Text = str;
+                        Fullname.Text = FullName;
+                    }
+                    else
+                    {
+                        sw.ReadLine();
+                    }
+                }
+
+                if (!temp)
+                {
+                    MessageBox.Show("Данный студент не был найден");
+                    return;
+                }
+
+
+            }
+
+        }
+    }
+}
---
+++ StudentBook.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Annotations;
+
+namespace WpfApp2
+{
+    public class StudentBook
+    {
+
+            int id;
+            string FullName;
+            private int n = 0;
+
+            struct Item
+            {
+                public string type;
+                public int result;
+                public int numberS;
+                public string ItemName;
+            }
+
+            List<Item> Predmets = new List<Item>();
+
+            public StudentBook(int id, string FullName)
+            {
+                this.id = id;
+                this.FullName = FullName;
+            }
+
+            public StudentBook()
+            {
+
+            }
+            public void Add(string type, int result, int numberS, string ItemName)
+            {
+                Item temp = new Item();
+                temp.type = type;
+                temp.result = result;
+                temp.numberS = numberS;
+                temp.ItemName = ItemName;
+                Predmets.Add(temp);
+                n++;
+            }
+
+            public void Vivod()
+            {
+                for (int i = 0; i < n; i++)
+                    Console.WriteLine(Predmets[i].numberS + " " + Predmets[i].result + " " + Predmets[i].type + " " + Predmets[i].ItemName);
+
+            }
+
+            public float average()
+            {
+            float average = 0;
+            float summ = 0;
+            for (int i = 0; i < n; i++)
+            {
+                summ += Predmets[i].result;
+            }
+            average = summ / n;
+            return average;
+            }
+            public string Stipendia()
+            {
+
+                int min = 5;
+                int max = 0;
+
+                for (int i = 0; i < n; i++)
+                {
+                    if ((Predmets[i].numberS > max))
+                    {
+                        max = Predmets[i].numberS;
+                    }
+                }
+
+            for (int i = 0; i < n; i++)
+                { 
+                    if (((Predmets[i].result < min) && ((Predmets[i].type=="Экзамен") || (Predmets[i].type=="Диф зачёт")) && (Predmets[i].numberS==max)))
+                    {
+                        min = Predmets[i].result;
+                    }
+                    else
+                    {
+                        return "Нет оценок, чтобы определить стипендию";
+                    }
+                }
+
+                if (min == 5)
+                {
+                    return "Повышенная стипендия";
+                }
+                if (min == 4)
+                {
+                    return "Стипендия не повышенная";
+                }
+                else
+                {
+                    return "Стипендии нет";
+                }
+
+
+            }
+
+
+            public string Diplom()
+            {
+                int max = 0;
+                float average = 0;
+                for (int i = 0; i < n; i++)
+                {
+                    if (Predmets[i].numberS > max)
+                    {
+                        max = Predmets[i].numberS;
+                    }
+                }
+                if (max < 10)
+                {
+                    for (int i = 0; i < n; i++)
+                    {
+                        if ((Predmets[i].result == 3) && ((Predmets[i].type=="Экзамен") || (Predmets[i].type=="Диф зачёт")))
+                        {
+                            return "Возможен только синий Диплом";
+                        }
+
+                        average += Predmets[i].result;
+
+                    }
+
+                    average = average / n;
+                    if (average >= 4.75)
+                    {
+                        return "Может быть красный диплом";
+                    }
+                    else
+                    {
+                        return "Возможет только синий Диплом";
+                    }
+                }
+                else
+                {
+                    return "Диплом уже есть";
+                }
+            }
+        }
+}
